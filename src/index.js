2f2102cd703e8f920b2b7bcc7a510ae3d7efad03
Craig Miller <craigmiller160@gmail.com>
2021-07-19 12:33:24 -0400

    Refactoring logging and input writing

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const DOMAIN = 'craigmiller160.ddns.net';
 const EMAIL = 'craigmiller160@gmail.com';
 const AUTHCODE_PATH = path.resolve(os.homedir(), 'authcode', 'authcode.txt');
 const YES = 'y';
+const INGRESS_CERT_PATH = '/opt/kubernetes/data/ingress/cert';
+const CERTBOT_CERT_PATH = `/etc/letsencrypt/live/${DOMAIN}`;
 
 const ENTER_EMAIL_PROMPT = /Enter email address/;
 const ENTER_DOMAIN_PROMPT = /enter in your domain name/;
@@ -18,12 +20,17 @@ const TERMS_OF_SERVICE_PROMPT = /Please read the Terms of Service/;
 
 let challengeReady = false;
 
+const sendInput = (shell, inputText) => {
+	console.log(inputText);
+	shell.sdtin.write(`${inputText}\n`);
+};
+
 const handleOutput = (shell, buffer) => {
 	const text = buffer.toString();
 	console.log(text);
 
 	if (ENTER_DOMAIN_PROMPT.test(text) || ENTER_DOMAIN_2_PROMPT.test(text)) {
-		shell.stdin.write(`${DOMAIN}\n`);
+		sendInput(shell, DOMAIN);
 	} else if (CREATE_FILE_PROMPT.test(text)) {
 		const prefixIndex = text.indexOf(':');
 		const promptMinusPrefix = text.substring(prefixIndex + 2);
@@ -32,13 +39,11 @@ const handleOutput = (shell, buffer) => {
 
 		fs.writeFileSync(AUTHCODE_PATH, promptMinusPrefixSuffix);
 		challengeReady = true;
-		shell.stdin.write('\n');
+		sendInput(shell, '');
 	} else if (ENTER_EMAIL_PROMPT.test(text)) {
-		console.log(EMAIL);
-		shell.stdin.write(`${EMAIL}\n`);
+		sendInput(shell, EMAIL);
 	} else if (TERMS_OF_SERVICE_PROMPT.test(text)) {
-		console.log(YES);
-		shell.stdin.write(`${YES}\n`);
+		sendInput(shell, YES);
 	}
 };
 
@@ -48,5 +53,5 @@ shell.stdout.on('data', (data) => handleOutput(shell, data));
 shell.stderr.on('data', (data) => handleOutput(shell, data));
 
 shell.on('close', () => {
-	console.log('Copy cert files to "/opt/kubernetes/data/ingress/cert"');
+	console.log(`Copying cert files from ${CERTBOT_CERT_PATH} to ${INGRESS_CERT_PATH}`);
 });
